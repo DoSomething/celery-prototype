4fa80810f63ef14988a635a371aab03d0b0c598a
Sergii Tkachenko <hi@sergii.org>
2017-06-26 16:20:34 -0400

    Send request ip address to print_params worker!

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import request
+from workers import print_params
 
 web = Flask(__name__)
 
 @web.route('/', methods=['GET', 'POST'])
 def blink():
+    print_params.apply_async(args=[request.remote_addr])
     return "Object Created", 201
 
 if __name__ == ("__main__"):
