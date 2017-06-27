8fce7359cf6258d401087f904d91ee57bd3724dc
Sergii Tkachenko <hi@sergii.org>
2017-06-27 15:44:21 -0400

    Re-enable monitoring

--- celeryconfig.py
+++ celeryconfig.py
@@ -6,5 +6,4 @@ broker_url = os.environ['BROKER_URI']
 broker_pool_limit = 1 # Will decrease connection usage
 broker_heartbeat = None # We're using TCP keep-alive instead
 broker_connection_timeout = 30 # May require a long timeout due to Linux DNS timeouts etc
-worker_send_task_events = False # Will not create celeryev.* queues
 event_queue_expires = 60 # Will delete all celeryev. queues without consumers after 1 minute.
