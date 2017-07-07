7f85dfd78eb94acd7705c00e9056d4c629a734bc
Sergii Tkachenko <hi@sergii.org>
2017-07-07 12:48:49 -0400

    Basic gambit_mdata_relay

--- app/web.py
+++ app/web.py
@@ -1,13 +1,26 @@
 from flask import Flask
-from .workers import print_params
+from app.workers import print_params
+from app.workers import gambit_mdata_relay
 
 SERVER = Flask(__name__)
 
-@SERVER.route('/', methods=['GET', 'POST'])
-def blink():
-    print_params.apply_async(args=[1])
-    return "Object Created", 201
 
-if __name__ == ("__main__"):
+@SERVER.route('/', methods=['GET'])
+def get_params():
+    print_params.apply_async(args=[1])
+    return 'Object Created', 201
+
+
+@SERVER.route('/requests', methods=['GET'])
+def get_requests():
+    data = {
+        'event_id': '1',
+        'event_type': '2'
+    }
+    gambit_mdata_relay.apply_async(args=[data])
+    return 'Object Created', 201
+
+
+if __name__ == ('__main__'):
     SERVER.debug = False
     SERVER.run()
--- app/workers.py
+++ app/workers.py
@@ -3,9 +3,15 @@ from celery import Celery
 CELERY = Celery()
 CELERY.config_from_object('celeryconfig')
 
+
 @CELERY.task(bind=True, default_retry_delay=1)
 def print_params(self, param):
     if param > 5:
         self.retry(max_retries=5)
     else:
         return True
+
+
+@CELERY.task()
+def gambit_mdata_relay(data):
+    print(data)
