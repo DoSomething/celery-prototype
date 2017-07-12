8a607fab7647ffec5557441a368a8bed371e49d7
Sergii Tkachenko <hi@sergii.org>
2017-07-12 17:36:54 -0400

    Configure celery from .env

--- run-workers.py
+++ run-workers.py
@@ -1,4 +1,10 @@
-from blink.celery import app
+import os
+
+from dotenv import find_dotenv, load_dotenv
+
+from blink.celery import get_app
+
 
 if __name__ == '__main__':
-    app.start()
+    load_dotenv(find_dotenv())
+    get_app().start()
