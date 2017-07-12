6e92ffae3715acfd1b98bb159262194e618572dd
Sergii Tkachenko <hi@sergii.org>
2017-07-12 11:01:57 -0400

    Prepare for presentation

--- app/workers.py
+++ app/workers.py
@@ -11,6 +11,7 @@ CELERY.config_from_object('celeryconfig')
 
 @CELERY.task(bind=True, default_retry_delay=1)
 def print_params(self, param):
+    print("Log log log")
     if param > 5:
         self.retry(max_retries=5)
     else:
