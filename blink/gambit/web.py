0f84461f5a641ef9ab20f929c01ff5f1534d0d6d
Sergii Tkachenko <hi@sergii.org>
2017-07-17 18:01:00 -0400

    Send 422 on json parsing error

--- blink/gambit/web.py
+++ blink/gambit/web.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import request
+from flask import jsonify
 
 from .tasks import gambit_mdata_relay
 
@@ -12,6 +13,15 @@ def index():
 
 @web.route('/mdata', methods=('POST',))
 def mdata():
-    data = request.get_json(force=True)
-    gambit_mdata_relay.apply_async(args=(data,))
-    return 'Done', 201
+    data = request.get_json(force=True, silent=True)
+    if data == None:
+        # Not parsed
+        return jsonify(jsonrpc=2.0), 422
+
+    task_result = gambit_mdata_relay.apply_async(args=(data,))
+    response = jsonify(
+        jsonrpc=2.0,
+        result='success_message_queued',
+        id=task_result.id,
+    )
+    return response, 201
